8ea81a2233f8aaf429f396f5d87bb441d10c6d6e
Gezer <oliveiracorp.dev01@gmail.com>
2022-05-12 09:11:48 -0300

    tradução ES adicionado

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #ebooks
 <p>Rodando fila e host em produção</p>
 <p>php artisan queue:work && serve --host=10.0.1.136</p>
+php artisan queue:work && serve --host=10.0.1.136
---
+++ lang/es/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return ['locale' => 'Locación inválida.',
+    'credentials' => 'Las credenciales proporcionadas son incorrectas.',
+    'current_password' => 'Las credenciales proporcionadas son incorrectas.',
+    'email_exists' => '¡El correo electrónico ya existe en nuestra base de datos!',
+    'password_last_equals'=>'Su nueva contraseña debe ser diferente de la contraseña utilizada anteriormente.',
+    'signature_invalid' => '¡Suscripción no válida!' ,
+];
